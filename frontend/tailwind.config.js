e370fe8e904df99e27497777f1408870f302ead5
Gerard Marquina Rubio <gerardmarquinarubio@gmail.com>
2022-09-07 14:15:05 +0200

    Init tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./layouts/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui')
+  ],
+}
